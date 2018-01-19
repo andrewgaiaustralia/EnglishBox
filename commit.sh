5be73d1ac5a26c6558d604b714b686e8283a0220
Alex Yin <fudanyinxin@gmail.com>
2018-01-19 23:34:22 +1100

    updated

--- commit.sh
+++ commit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-cd /Users/alexyin/EnglishBox/
+#cd /Users/alexyin/EnglishBox/
+cd /c/Alex/EnglishBox
+git config --global user.email "fudanyinxin@gmail.com"
+git config --global user.name "Alex Yin"
 git add .
 git commit -am "updated"
 git push https://orochigalois:Miaoyaliu1213@github.com/orochigalois/EnglishBox.git master
