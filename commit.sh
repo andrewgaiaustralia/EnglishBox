8a16e746b2e5a44f0e8dd116ffccce1d144c1fd3
Andrew Gai <andrew.gai.australia@gmail.com>
2019-01-09 17:55:14 +1100

    updated

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd /Users/alex/EnglishBox/
+cd /Users/hongxungai/EnglishBox/
 #cd /c/Alex/EnglishBox
-git config --global user.email "fudanyinxin@gmail.com"
-git config --global user.name "Alex Yin"
+git config --global user.email "andrew.gai.australia@gmail.com"
+git config --global user.name "Andrew Gai"
 git add .
 git commit -am "updated"
-git push https://orochigalois:Miaoyaliu1213@github.com/orochigalois/EnglishBox.git master
+git push https://andrewgaiaustralia:australiagaiandrew@github.com/orochigalois/EnglishBox.git master
